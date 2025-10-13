b52172c9fa0c9f6f5e1ec2c00b349122e5c8c103
BrendanGlancy <bglance68@gmail.com>
2025-10-13 17:02:02 -0400

    we defineatly rendered something

--- term.c
+++ term.c
@@ -1,3 +1,9 @@
+#ifdef __APPLE__
+#include <util.h>
+#else
+#include <bits/types/struct_timeval.h>
+#include <pty.h>
+#endif
 #include "lib/window.h"
 #include <GLFW/glfw3.h>
 #include <limits.h>
@@ -10,16 +16,17 @@
 #include <sys/select.h>
 #include <unistd.h>
 
-#ifdef __APPLE__
-    #include <util.h>
-#else
-    #include <pty.h>
-    #include <bits/types/struct_timeval.h>
-#endif
+#define TERM_COLS 80
+#define TERM_ROWS 24
 
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
+// master file descriptor
 static int32_t masterfd;
+
+static uint32_t screen[TERM_ROWS][TERM_COLS]; // 2D array of codepoints
+static int cursor_x = 0;
+static int cursor_y = 0;
 
 // takes a point to a UTF-8 byte sequence and decodes a single Unicode codepoint
 // from it.
@@ -55,9 +62,23 @@ size_t readfrompty(void) {
     while (iter < buflen) {
         uint32_t codepoint;
         int32_t len = utf8decode(&buf[iter], &codepoint);
+
         if (len == -1 || len > buflen)
             break;
-        printf("%i\n", codepoint);
+
+        if (codepoint == 10) {
+            cursor_x = 0;
+            cursor_y++;
+        } else {
+            screen[cursor_y][cursor_x] = codepoint;
+            cursor_x++;
+
+            if (cursor_x >= TERM_COLS) { // wrap to next line
+                cursor_x = 0;
+                cursor_y++;
+            }
+        }
+
         iter += len;
     }
 
@@ -66,6 +87,27 @@ size_t readfrompty(void) {
     }
     buflen -= iter;
     return nbytes;
+}
+
+void render_terminal(void) {
+    float char_width = 10.0f;
+    float char_height = 20.0f;
+
+    for (int y = 0; y < TERM_ROWS; y++) {
+        for (int x = 0; x < TERM_COLS; x++) { 
+            uint32_t cp = screen[x][y];
+            if (cp == 0) {
+                continue;
+            }
+            // calculate pixel position
+            float pixel_x = x * char_width;
+            float pixel_y = y * char_height;
+
+            // convert codepoint to string
+            char str[2] = {(char)cp, '\0'};
+            window_draw_text(pixel_x, pixel_y, str);
+        }
+    }
 }
 
 // creates a pty and fork + event loop
@@ -101,7 +143,7 @@ int main(void) {
         }
 
         window_clear(0.05f, 0.05f, 0.06f);
-        window_draw_text(10.0f, 20.0f, "Hello World");
+        render_terminal();
         window_swap();
 
         glfwWaitEventsTimeout(0.01);
